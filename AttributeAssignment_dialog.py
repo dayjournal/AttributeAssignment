4c6e5426da6a839032561ac5ea35def363c5e992
Matthias Kuhn <matthias@opengis.ch>
2020-11-14 12:00:32 +0100

    Use configured edit widget instead of plain text

--- AttributeAssignment_dialog.py
+++ AttributeAssignment_dialog.py
@@ -14,12 +14,53 @@
 """
 
 import os
+import warnings
 
-from PyQt5 import uic, QtWidgets, QtCore
-from qgis.gui import *
-from .AttributeAssignment_base import Ui_AttributeAssignment
+from qgis.PyQt.uic import loadUiType
+from PyQt5.QtWidgets import QDialog, QHBoxLayout
 
-class AttributeAssignmentDialog(QtWidgets.QDialog, Ui_AttributeAssignment):
+
+def get_ui_class(ui_file):
+    """Get UI Python class from .ui file.
+       Can be filename.ui or subdirectory/filename.ui
+    :param ui_file: The file of the ui in svir.ui
+    :type ui_file: str
+    """
+    rel_file_path = os.path.join(os.path.dirname(__file__), 'ui', ui_file)
+    ui_file_path = os.path.abspath(rel_file_path)
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        return loadUiType(ui_file_path)[0]
+
+DIALOG_UI = get_ui_class('assignment_dialog.ui')
+
+class AttributeAssignmentDialog(QDialog, DIALOG_UI):
     def __init__(self, parent=None):
         super(AttributeAssignmentDialog, self).__init__(parent)
-        self.setupUi(self)      
+        self.setupUi(self)
+
+        self.mMapLayerComboBox.layerChanged.connect(self.mFieldComboBox.setLayer)
+        self.mFieldComboBox.fieldChanged.connect(self.currentFieldChanged)
+        self.layout = QHBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.mValuePlaceholder.setLayout(self.layout)
+
+        self.wrapper = None
+        self.widget = None
+
+    def currentFieldChanged(self, fieldName):
+        from qgis.gui import QgsGui
+        reg = QgsGui.editorWidgetRegistry()
+
+        if self.widget:
+            self.layout.removeWidget(self.widget)
+            self.widget = None
+
+        if fieldName:
+            fieldIndex = self.mMapLayerComboBox.currentLayer().fields().indexFromName(fieldName)
+            self.wrapper = reg.create(self.mMapLayerComboBox.currentLayer(), fieldIndex, None, self.mValuePlaceholder)
+            self.widget = self.wrapper.widget()
+            self.layout.addWidget(self.widget)
+        else:
+            self.wrapper = None
